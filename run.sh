1f0e7adce456bda7d2d9c221a1b567db277d91d2
Bruno Hildenbrand <bruno@local>
2024-07-03 17:26:32 -0600

    Add httping for Ubuntu 24.04

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# avoid reusing previously successful rock
+rm *.rock
+
 echo "Packing our rock..."
 rockcraft pack
 
-printf "\n\nUploading our rock to our local Docker registry...\n"
+printf "\nUploading our rock to our local Docker registry...\n"
 rockcraft.skopeo --insecure-policy copy oci-archive:httping_latest_amd64.rock docker-daemon:httping:latest
 
-printf "\n\nRunning our rock...\n"
+printf "\nRunning our rock...\n"
 docker run --rm httping:latest exec httping -c 3 google.com
 

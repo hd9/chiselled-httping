183455ac8b8e4011c96961753febca61ba4616cb
Bruno Hildenbrand <bruno@local>
2024-07-03 14:47:24 -0600

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Packing our rock
 echo "Packing our rock..."
 rockcraft pack
 
-# Uploading our rock to Docker
+printf "\n\nUploading our rock to our local Docker registry...\n"
 rockcraft.skopeo --insecure-policy copy oci-archive:httping_latest_amd64.rock docker-daemon:httping:latest
 
-# Run our rock
+printf "\n\nRunning our rock...\n"
 docker run --rm httping:latest exec httping -c 3 google.com
 
